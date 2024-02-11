d6e91c2f5dfd1d1332acfa48f1d735012640f1f0
Ajeenasics <ajeena.sics@gmail.com>
2024-02-11 19:15:42 +0530

    first commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from "../Components/Header/Header"
+import ProductTile from "../Components/product/productTile"
+import Footer from "../Components/footer/footer"
+
+function Home() {
+  return (
+    <div>
+      <Header/>
+      <ProductTile/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Home
